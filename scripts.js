c1d75b73e8c2708cbd45ee09ff0bca97b6a3bcdd
Kevin Phan <57550661+kphanplan@users.noreply.github.com>
2020-09-20 13:48:09 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,7 @@
 const app = document.getElementById('root')
 
-const logo = document.createElement('img')
-logo.src = 'Images/logo.png'
-
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
-
-app.appendChild(logo)
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
